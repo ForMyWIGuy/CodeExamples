7011f1d9662aa145f87102f871f33d51d6a7025f
ForMyWIGuy <45777162+ForMyWIGuy@users.noreply.github.com>
2024-08-16 12:48:29 -0500

    BinaryGap.py

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,49 +1,17 @@
+### Codility exercise: BinaryGap
+# Full exercise description  https://app.codility.com/programmers/lessons/1-iterations/binary_gap/ 
+# Evaluation of my solution  https://app.codility.com/demo/results/trainingXHXZEN-5XU/
+
+# Find longest sequence of zeros in binary representation of an integer.
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jul 12 12:05:12 2023
-
 @author: tiffanyberg
 """
 
 def my_function(N):
-    
-    # N = input("Provide a number\n")
-    # n = int(6)
-    # bin_n = bin(n)
-    # bin_n = bin_n[2:]
-    
-    # print(n, bin_n)
-    # len(bin_n)
-    # sub1 = '1'
-    # one_count = bin_n.count('1')
-    # if one_count >= 2:
-    #     a = bin_n.split(sub1)
-    #     # a[1]
-    # #else:
-    #     #print('0')
-    #     #return(0)
-        
-    # # A = []
-    # current_max_count = 0
-    # for item in a:
-    #     # print(item)
-    #     current_count_new = item.count('0')
-    #     if current_count_new > current_max_count:
-    #         current_max_count = current_count_new
-    # print("Final Answer:", current_max_count)
-    # #return(current_max_count)
-        
-        
-    #     if item.isdigit() == True:
-    #         A.append(item)
-        
-    # # len(A)
-    # for each in bin_n:
-    #     if bin_n[each] == 1:
-    #         print("Hello")
-    
-    #### Effective 100% correct answer begins here
     B = []    
     binary_gap = 0
     my_num = str(bin(N)[2:])
@@ -73,8 +41,8 @@ def my_function(N):
     else:
         binary_gap = max_difference - 1
         return(binary_gap)
-
-my_function(6) ##Does not exclude trailing Zero
-my_function(328) ##Does not exclude trailing Zero
-my_function(135)
-my_function(23415)
+## tests
+# my_function(6) ##Does not exclude trailing Zero
+# my_function(328) ##Does not exclude trailing Zero
+# my_function(135)
+# my_function(23415)
